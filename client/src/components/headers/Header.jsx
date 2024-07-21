b1752a3bc01984eb1f1b059bc9015f4327aab77e
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-07-22 00:12:29 +0300

    Change register form to controlled

--- client/src/components/headers/Header.jsx
+++ client/src/components/headers/Header.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../../context/authContext";
 
 
 export default function Header() {
+    const {
+        isAuthenticated,
+        username,
+    } = useContext(AuthContext);
+
     return(
         <header>
         {/* Navigation */}
@@ -11,15 +18,22 @@ export default function Header() {
             <nav>
                 <Link to="/games">All games</Link>
                 {/* Logged-in users */}
-                <div id="user">
-                    <Link to="/games/create">Create Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
+                {isAuthenticated && (
+                    <div id="user">
+                        <Link to="/games/create">Create Game</Link>
+                        <Link to="/logout">Logout</Link>
+                        <span>| {username}</span>
+                    </div>
+                )}
+
                 {/* Guest users */}
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {!isAuthenticated && (
+                    <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>
+                )}
+                
             </nav>
         </header>
     );
