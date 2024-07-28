a42f72cb735fcfcd00b0269b8614000b555cc3e0
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-07-28 20:30:54 +0300

    Add error boundary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,30 +12,34 @@ import GameList from './components/game-list/GameList';
 import GameDetails from './components/game-details/gameDetails';
 import Logout from './components/logout/Logout';
 import GameEdit from './components/game-edit/GameEdit';
+import ErrorBoundary from './components/errorBoundary';
 
 
 
 function App() {
 
-  return (
-    <AuthProvider>
-      <div id="box">
-        <Header />
+    return (
+        <ErrorBoundary>
+            <AuthProvider>
+                <div id="box">
+                    <Header />
 
-        <Routes>
-          <Route path={Path.Home} element={<Home />} />
-          <Route path="/games" element={<GameList />} />
-          <Route path="/games/create" element={<GameCreate />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/games/:gameId" element={<GameDetails />} />
-          <Route path={Path.GameEdit} element={<GameEdit />} />
-          <Route path={Path.Logout} element={<Logout />} />
-        </Routes>
+                    <Routes>
+                        <Route path={Path.Home} element={<Home />} />
+                        <Route path="/games" element={<GameList />} />
+                        <Route path="/games/create" element={<GameCreate />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/games/:gameId" element={<GameDetails />} />
+                        <Route path={Path.GameEdit} element={<GameEdit />} />
+                        <Route path={Path.Logout} element={<Logout />} />
+                    </Routes>
 
-      </div>
-    </AuthProvider>
-  )
+                </div>
+            </AuthProvider>
+        </ErrorBoundary>
+
+    )
 }
 
 export default App
