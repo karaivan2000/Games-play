7198ef1b56013c8859c05c4d93d65dfd437028e8
karaivan2000 <133887129+karaivan2000@users.noreply.github.com>
2024-08-06 04:48:21 +0300

    c

--- client/src/components/game-details/gameDetails.jsx
+++ client/src/components/game-details/gameDetails.jsx
@@ -62,66 +62,266 @@ export default function GameDetails() {
     const isOwner = userId === game._ownerId;
 
     return (
-        <section id="game-details">
-            <h1>Game Details</h1>
-            <div className="info-section">
-                <div className="game-header">
-                    <img className="game-img" src={game.imageUrl} />
-                    <h1>{game.title}</h1>
-                    <span className="levels">MaxLevel: {game.maxLevel}</span>
-                    <p className="type">{game.category}</p>
-                </div>
-                <p className="text">
-                    {game.summary}
-                </p>
-                {/* Bonus ( for Guests and Users ) */}
-                <div className="details-comments">
-                    <h2>Comments:</h2>
-                    <ul>
-                        {/* list all comments for current game (If any) */}
-                        {comments.map(({ _id, text, owner: { email } }) => (
-                            <li key={_id} className="comment">
-                                <p>{email}: {text}</p>
-                            </li>
-                        ))}
-
-                    </ul>
-                    {/* Display paragraph: If there are no games in the database */}
-                    {comments.length === 0 && (
-                        <p className="no-comment">No comments.</p>
-                    )}
-                </div>
-                {/* Edit/Delete buttons ( Only for creator of this game )  */}
-                {isOwner && (
-                    <div className="buttons">
-                        <Link to={pathToUrl(Path.GameEdit, { gameId })} className="button">
-                            Edit
-                        </Link>
-                        <button className="button" onClick={deleteButtonClickHandler}>Delete</button>
-                    </div>
-                )}
-
+<>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Recipe</title>
+  <style
+    dangerouslySetInnerHTML={{
+      __html:
+        "\n        .recipe, #comments {\n            display: none; /* Initially hidden */\n        }\n        .btn {\n            display: inline-block;\n            padding: 10px 20px;\n            font-size: 16px;\n            cursor: pointer;\n            color: #fff;\n            background-color: #007BFF;\n            border: none;\n            border-radius: 5px;\n            text-align: center;\n            margin-top: 20px;\n        }\n    "
+    }}
+  />
+  {/*row*/}
+  <div className="row">
+    <header className="s-title">
+      <h1>A luxurious black &amp; white chocolate cupcake</h1>
+    </header>
+    {/*content*/}
+    <section className="content three-fourth">
+      {/*button*/}
+      <button className="btn" id="seeMoreBtn">
+        See More
+      </button>
+      {/*recipe*/}
+      <div className="recipe">
+        <div className="row">
+          {/*two-third*/}
+          <article className="two-third">
+            <div className="image">
+              <a href="#">
+                <img src="images/img.jpg" alt="" />
+              </a>
             </div>
-            {/* Bonus */}
-            {/* Add Comment ( Only for logged-in users, which is not creators of the current game ) */}
-            <article className="create-comment">
-                <label>Add new comment:</label>
-                <form className="form" onSubmit={onSubmit}>
+            <div className="intro">
+              <p>
+                <strong>
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas
+                </strong>
+              </p>
+              <p>
+                Molestias excepturi sint occaecati cupiditate non provident,
+                similique sunt in culpa qui officia deserunt mollitia animi, id
+                est laborum et dolorum fuga. Et harum quidem rerum facilis est
+                et expedita distinctio. At vero eos et accusamus et iusto odio
+                dignissimos ducimus qui blanditiis praesentium voluptatum
+                deleniti atque corrupti quos dolores et quas molestias excepturi
+                sint occaecati cupiditate non provident, similique sunt in culpa
+                qui officia deserunt mollitia animi, id est laborum et dolorum
+                fuga. Et harum quidem rerum facilis est et expedita distinctio.
+              </p>
+            </div>
+            <div className="instructions">
+              <ol>
+                <li>
+                  Heat oven to 160C/140C fan/gas 3 and line a 12-hole muffin tin
+                  with cases. Gently melt the butter, chocolate, sugar and 100ml
+                  hot water together in a large saucepan, stirring occasionally,
+                  then set aside to cool a little while you weigh the other
+                  ingredients.
+                </li>
+                <li>
+                  Stir the eggs and vanilla into the chocolate mixture. Put the
+                  flour into a large mixing bowl, then stir in the chocolate
+                  mixture until smooth. Spoon into cases until just over
+                  three-quarters full (you may have a little mixture leftover),
+                  then set aside for 5 mins before putting on a low shelf in the
+                  oven and baking for 20-22 mins. Leave to cool.
+                </li>
+                <li>
+                  For the icing, melt the chocolate in a heatproof bowl over a
+                  pan of barely simmering water. Once melted, turn off the heat,
+                  stir in the double cream and sift in the icing sugar. When
+                  spreadable, top each cake with some and decorate with your
+                  favourite sprinkles and sweets.
+                </li>
+              </ol>
+            </div>
+          </article>
+          {/*//two-third*/}
+          {/*one-third*/}
+          <article className="one-third">
+            <dl className="basic">
+              <dt>Preparation time</dt>
+              <dd>15 mins</dd>
+              <dt>Cooking time</dt>
+              <dd>30 mins</dd>
+              <dt>Difficulty</dt>
+              <dd>easy</dd>
+              <dt>Serves</dt>
+              <dd>4 people</dd>
+            </dl>
+            <dl className="user">
+              <dt>Category</dt>
+              <dd>Deserts</dd>
+              <dt>Posted by</dt>
+              <dd>Jennifer W.</dd>
+            </dl>
+            <dl className="ingredients">
+              <dt>300g</dt>
+              <dd>Self-raising flour</dd>
+              <dt>200g</dt>
+              <dd>Butter</dd>
+              <dt>200g</dt>
+              <dd>Plain chocolate</dd>
+              <dt>2</dt>
+              <dd>Eggs</dd>
+              <dt>1 tbsp</dt>
+              <dd>Vanilla extract</dd>
+              <dt>200 g</dt>
+              <dd>Brown sugar</dd>
+              <dt>100 ml</dt>
+              <dd>Double cream</dd>
+              <dt>handful</dt>
+              <dd>Sprinkles</dd>
+            </dl>
+          </article>
+          {/*//one-third*/}
+        </div>
+      </div>
+      {/*//recipe*/}
+      {/*comments*/}
+      <div className="comments" id="comments">
+        <h2>5 comments </h2>
+        <ol className="comment-list">
+          {/*comment*/}
+          <li className="comment depth-1">
+            <div className="avatar">
+              <a href="my_profile.html">
+                <img src="images/avatar.jpg" alt="" />
+              </a>
+            </div>
+            <div className="comment-box">
+              <div className="comment-author meta">
+                <strong>Kimberly C.</strong> said 1 month ago{" "}
+                <a href="#" className="comment-reply-link">
+                  {" "}
+                  Reply
+                </a>
+              </div>
+              <div className="comment-text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                  aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                  nostrud exerci tation.
+                </p>
+              </div>
+            </div>
+          </li>
+          {/*//comment*/}
+          {/*comment*/}
+          <li className="comment depth-1">
+            <div className="avatar">
+              <a href="my_profile.html">
+                <img src="images/avatar.jpg" alt="" />
+              </a>
+            </div>
+            <div className="comment-box">
+              <div className="comment-author meta">
+                <strong>Alex J.</strong> said 1 month ago{" "}
+                <a href="#" className="comment-reply-link">
+                  {" "}
+                  Reply
+                </a>
+              </div>
+              <div className="comment-text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                  aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                  nostrud exerci tation.
+                </p>
+              </div>
+            </div>
+          </li>
+          {/*//comment*/}
+          {/*comment*/}
+          <li className="comment depth-2">
+            <div className="avatar">
+              <a href="my_profile.html">
+                <img src="images/avatar.jpg" alt="" />
+              </a>
+            </div>
+            <div className="comment-box">
+              <div className="comment-author meta">
+                <strong>Kimberly C.</strong> said 1 month ago{" "}
+                <a href="#" className="comment-reply-link">
+                  {" "}
+                  Reply
+                </a>
+              </div>
+              <div className="comment-text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                  aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                  nostrud exerci tation.
+                </p>
+              </div>
+            </div>
+          </li>
+          {/*//comment*/}
+          {/*comment*/}
+          <li className="comment depth-3">
+            <div className="avatar">
+              <a href="my_profile.html">
+                <img src="images/avatar.jpg" alt="" />
+              </a>
+            </div>
+            <div className="comment-box">
+              <div className="comment-author meta">
+                <strong>Alex J.</strong> said 1 month ago{" "}
+                <a href="#" className="comment-reply-link">
+                  {" "}
+                  Reply
+                </a>
+              </div>
+              <div className="comment-text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                  aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                  nostrud exerci tation.
+                </p>
+              </div>
+            </div>
+          </li>
+          {/*//comment*/}
+          {/*comment*/}
+          <li className="comment depth-1">
+            <div className="avatar">
+              <a href="my_profile.html">
+                <img src="images/avatar.jpg" alt="" />
+              </a>
+            </div>
+            <div className="comment-box">
+              <div className="comment-author meta">
+                <strong>Denise M.</strong> said 1 month ago{" "}
+                <a href="#" className="comment-reply-link">
+                  {" "}
+                  Reply
+                </a>
+              </div>
+              <div className="comment-text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                  aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                  nostrud exerci tation.
+                </p>
+              </div>
+            </div>
+          </li>
+        </ol>
+      </div>
+    </section>
+  </div>
+</>
 
-                    <textarea
-                        name="comment"
-                        value={values.comment}
-                        onChange={onChange}
-                        placeholder="Comment......"
-                        defaultValue={""}
-                    />
-                    <input
-                        className="btn submit"
-                        type="submit"
-                        defaultValue="Add Comment"
-                    />
-                </form>
-            </article>
-        </section>
     );
 }
+
+
